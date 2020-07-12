b680a8cb52eede92304af5220e4de7473bb4044d
Ryan Shirley <ryanjshirley@gmail.com>
2020-07-12 15:01:33 +0100

    Create user record on signup

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,29 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+
+admin.initializeApp()
+
+const db = admin.firestore()
+
+/**
+ * Creates a document with ID -> uid in the `Users` collection.
+ *
+ * @param {Object} userRecord Contains the auth, uid and displayName info.
+ * @param {Object} context Details about the event.
+ */
+const createProfile = (userRecord, context) => {
+    const { email, phoneNumber, uid } = userRecord
+
+    return db
+        .collection("Users")
+        .doc(uid)
+        .set({ email, displayName })
+        .catch(console.error)
+}
+
+module.exports = {
+    authOnCreate: functions.auth.user().onCreate(createProfile),
+}
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
