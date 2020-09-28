e8b91915464d5a89623233cda043e0affda9266c
Ryan Shirley <N00161198@student.iadt.ie>
2020-09-28 20:20:22 +0100

    Minus 7 days to get start of week

--- functions/index.js
+++ functions/index.js
@@ -77,7 +77,7 @@ const getMinerEarnings = functions.region('europe-west2').pubsub.schedule('0 0 *
             unpaid: convertToETH(unpaid),
             reportedHashrate: convertHToMH(reportedHashrate),
             todaysPayouts: todaysPayouts.reduce((accumulator, currentValue) => accumulator + currentValue),
-            today: now,
+            today: now.diff(7, 'days'),
             price
         }
 
